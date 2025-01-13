c4d7d2b0422265ac300ca1d47f866fa82512b7aa
xphip <xphipme@gmail.com>
2025-01-13 10:09:41 -0300

    fix: improve routes security ownership

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -11,28 +11,29 @@ import { IndexController } from "../http/controllers/indexController";
 import { LoginAuthController } from "../http/controllers/authLoginController";
 import { RegisterAuthController } from "../http/controllers/authRegisterController";
 import {ValidateOwnership} from "../http/middlewares/validateOwnershipMiddleware";
+import {ValidateIfIsAdmin} from "../http/middlewares/validateIfIsAdminMiddleware";
 
 const rootRoutes: Router = express.Router();
 
 // Private
 {
     rootRoutes.get("/users", [ValidateAuth], ListUsersController);
-    rootRoutes.get("/users/:id", [ValidateAuth], GetUsersController);
+    rootRoutes.get("/users/:id", [ValidateAuth, ValidateOwnership], GetUsersController);
     rootRoutes.post("/users/:id", [ValidateAuth, ValidateOwnership], CreateUsersController);
     rootRoutes.put("/users/:id", [ValidateAuth, ValidateOwnership], UpdateUsersController);
     rootRoutes.delete("/users/:id", [ValidateAuth, ValidateOwnership], DeleteUsersController);
 }
 {
     rootRoutes.get("/tasks", [ValidateAuth], ListTasksController);
-    rootRoutes.get("/tasks/:id", [ValidateAuth], GetTasksController);
+    rootRoutes.get("/tasks/:id", [ValidateAuth, ValidateOwnership], GetTasksController);
     rootRoutes.post("/tasks/:id", [ValidateAuth, ValidateOwnership], CreateTasksController);
     rootRoutes.put("/tasks/:id", [ValidateAuth, ValidateOwnership], UpdateTasksController);
     rootRoutes.delete("/tasks/:id", [ValidateAuth, ValidateOwnership], DeleteTasksController);
 }
 {
-    rootRoutes.get("/tokens/:id", [ValidateAuth], GetTokensController);
-    rootRoutes.post("/tokens/:id", [ValidateAuth, ValidateOwnership], CreateTokensController);
-    rootRoutes.delete("/tokens/:id", [ValidateAuth, ValidateOwnership], DeleteTokensController);
+    rootRoutes.get("/tokens/:id", [ValidateAuth, ValidateIfIsAdmin], GetTokensController);
+    rootRoutes.post("/tokens/:id", [ValidateAuth, ValidateIfIsAdmin], CreateTokensController);
+    rootRoutes.delete("/tokens/:id", [ValidateAuth, ValidateIfIsAdmin], DeleteTokensController);
 }
 
 // Public
