44febf190afa0112a55b5f80ec5120beb3fe6404
xphip <xphipme@gmail.com>
2025-01-13 10:09:07 -0300

    feat: add Admin middleware validator

--- backend/src/http/requestValidators.ts
+++ backend/src/http/requestValidators.ts
@@ -2,5 +2,10 @@ import {RequestCustom, SessionType} from "./types";
 
 export function IsOwnership(req: RequestCustom) {
     const session = req?.session as SessionType;
-    return !(session.role !== "admin" || session.id !== req.body.id);
+    return !(IsAdmin(req) || session.id !== req.body.id);
+}
+
+export function IsAdmin(req: RequestCustom) {
+    const session = req?.session as SessionType;
+    return !(session.role !== "admin");
 }
